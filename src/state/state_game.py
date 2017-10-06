0135df7016c0cffbd66bcbc29fd70f1ba318a119
Azel Ramajanine <sacdesable123@gmail.com>
2017-10-06 18:56:28 +0200

    Quelques modifs sur le state_game pour le faire coller aux primitives de clément

--- src/state/state_game.py
+++ src/state/state_game.py
@@ -15,33 +15,34 @@ class StateGame(GameState):
     def get_event(self,event):
         GameState.get_event(self,event)
         if event.type== KEYDOWN:
+            #Let's try to make the player jump by modifiying its velocity after checking if it's on the ground
             if event.key==K_SPACE:
-                if game_map.isOnTheGround(self.player.pos_x,self.player.pos_y,self.player.hitbox):
+                if game_map.on_the_ground(self.player.pos_x,self.player.pos_y,self.player.hitbox):
                     self.player.v_y=max(10,self.player.v_y)
-                    #Let's try to make the player jump by modifiying its velocity after checking if it's on the ground
+
 
     def update(self):
         Game.update(self)
         
-        #On met à jour la position en fonction des collisions
+        #Update of the pos
         x0=self.player.pos_x
-        x,y,is_the_game_over=self.game_map.move(self.player.pos_x,self.player.pos_y,self.player.v_x,self.player.v_y,self.player.hitbox)
+        (x,y),is_the_game_over=self.game_map.move_test(self.player.pos_x,self.player.pos_y,self.player.hitbox,self.player.v_x,self.player.v_y)
         self.player.pos_x=x
         self.player.pos_y=y
 
+        #Something to do in case the game is over
+        if is_the_game_over:
+            self.done=True
 
         #Update depending on whether the player is on the ground or not
-        if self.game_map.isOnTheGround(self.player.pos_x,self.player.pos_y,self.player.hitbox):
+        if self.game_map.on_the_ground(self.player.pos_x,self.player.pos_y,self.player.hitbox):
             self.player.action=action.RUNNING
             self.player.v_y=max(self.player.v_y,0)
         else:
             self.player.action=action.JUMPING
-            self.player.v_y=self.player.v_y+=self.acceleration_y
-        
+            self.player.v_y=max(self.player.v_y+self.acceleration_y,1000)
+        #Update of the game_map
         game_map.update(x-x0)
-
-        return
-        
 
     def draw(self,surface):
         GameState.draw(self,surface)
