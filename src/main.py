c23f19397223536c4929dc92329f6b7f7d0b79c9
ClementLalanne <clement.lalanne@ens.fr>
2017-10-06 15:36:56 +0200

    no notification

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-class player:
-    """The class for a player"""
+class game_object:
+    """An object for simple objects"""
+    __init__(self):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.v_x = 0
+        self.v_y = 0
+        self.hitbox = [(0,0)]
+
+class player(game_object):
+    __init__(self):
+        game_object.__init__(self)
 
 class god:
     """Responsible for the actualisation of things"""
