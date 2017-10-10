3504ce6d77678dec70be9a9db1d7346e9356fb20
Clément Lalanne <clement.lalanne@ens.fr>
2017-10-10 15:20:25 +0200

    functions added and hitbox modified

--- src/map.py
+++ src/map.py
@@ -16,25 +16,40 @@ class Map():
         """
         self.width = 600
         self.length = 2400
+        self.display_length = 800
         self.pos = 0
+        self.gen = 0
+        self.data = np.zeros((self.width, self.length))
 
     def on_the_ground(self, x0, y0, hitbox):
         """
         Returns a boolean indicating if the object given by pos0 ans his hitbox is on the ground
         """
-        pass
+        test = False
+        for x in range(hitbox[0]):
+            test = test or (self.data[x0 + x + self.pos, y0 + hitbox[1]] == Material.GROUND
+        return test
 
     def move_test(self, x0, y0, hitbox, dx, dy):
         """
-        test if a given movement is possible and returns the tuple of his new position and the boolean saying if he is dead during this movement
+        Tests if a given movement is possible and returns the tuple of his new position and the boolean saying if he is dead during this movement
         """
         pass
+
+    def gen_proc(self):
+        """
+        Launches a procedural generation for the map.
+        """
+        for i in range(self.display_length):
+            self.data[(self.gen + i) % slef.length, self.width - 1] = Material.GROUND
 
     def update(self, dx):
         """
         Updates the position of the map between two frames with speed dx/frame
         """
-        pass
+        if self.gen >= self.pos and (self.gen - self.pos) < self.display_length or self.gen <= self.pos and (self.length - self.pos) + self.gen < self.display_length:
+            self.gen_proc()
+        self.pos = self.pos + dx
 
     def display(self, surface):
         """
