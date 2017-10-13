74111b13b3bdabba5f235018a16b84de95c48ed8
Hector <sacdesable123@gmail.com>
2017-10-13 17:50:40 +0200

    Simplification des méthodes

--- src/state/state_game.py
+++ src/state/state_game.py
@@ -13,10 +13,10 @@ class StateGame(GameState):
 
     def __init__(self):
         GameState.__init__(self)
-        self.player = Player(0, 0, 1, 0)
+        self.player = Player(5, 0, 1, 0)
         self.game_map = Map()
         self.acceleration_x = 0  # As said, x variables aint of any use at the moment
-        self.acceleration_y = 2
+        self.acceleration_y = 3
         self.frame = 0  # Number of frame since begininng
         self.max_speed = 1000
         self.next_state="MAIN_MENU"
@@ -30,7 +30,7 @@ class StateGame(GameState):
             # Let's try to make the player jump by modifiying its velocity after checking if it's on the ground
             if event.key == pg.K_SPACE:
                 if self.game_map.on_the_ground(self.player.pos_x, self.player.pos_y, self.player.hitbox):
-                    self.player.v_y = min( -10, self.player.v_y)
+                    self.player.v_y = min(-25, self.player.v_y)
                     # Player get an ascending phase that lasts some frame where he can still gain some vertical velocity
                     self.player.action = Action.ASCEND
                     self.player.last_jump = self.frame
@@ -66,13 +66,13 @@ class StateGame(GameState):
         if self.game_map.on_the_ground(self.player.pos_x, self.player.pos_y, self.player.hitbox) and self.player.action != Action.ASCEND:
             self.player.action = Action.RUNNING
             self.player.v_y = min(self.player.v_y, 0)
-        elif self.player.action == Action.JUMPING or (self.player.action == Action.ASCEND and self.frame - self.player.last_jump > 30):
+        elif self.player.action == Action.JUMPING or (self.player.action == Action.ASCEND and self.frame - self.player.last_jump > 10):
             # Either is the player in jump state, or he stopped his ascension
             self.player.action = Action.JUMPING
             self.player.v_y = max(min(self.player.v_y + self.acceleration_y, self.max_speed), -self.max_speed)
         elif self.player.action == Action.ASCEND:
             # In that case, the player continues his ascension
-            self.player.v_y = max(min(self.player.v_y + self.acceleration_y - 2, self.max_speed), -self.max_speed)
+            self.player.v_y = max(min(self.player.v_y + self.acceleration_y - 3, self.max_speed), -self.max_speed)
 
 
         # Update of the game_map
