b9e10e87e2135150110fdb27936d0322a43e4509
Joseph Marotte <joseph.marotte@clipper.ens.fr>
2017-10-14 17:05:37 +0200

    ajout du l'etat pause, et ajout de la transition du jeu à la pause , en gardant l'écran de la partie en cours en fond

---
+++ src/state/state_pause.py
@@ -0,0 +1,40 @@
+from state_engine import GameState
+import pygame as pg
+
+
+class Pause(GameState):
+
+    def __init__(self):
+        GameState.__init__(self)
+        self.available_state = ["GAME", "MAIN_MENU"]
+        self.current_select = 0
+        self.next_state = None
+
+    def startup(self, persistent):
+        pass
+
+    def get_event(self, event):
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+                self.current_select = (self.current_select - 1) % len(self.available_state)
+            elif event.key == pg.K_DOWN:
+                self.current_select = (self.current_select + 1) % len(self.available_state)
+            elif event.key == pg.K_RETURN:
+                self.next_state = self.available_state[self.current_select]
+                self.done = True
+
+    def update(self, dt):
+        pass
+
+    def draw(self, surface):
+        width, height = surface.get_size()
+
+        for i, name_state in enumerate(self.available_state):
+            if i == self.current_select:
+                text_color = 255, 0, 0
+            else:
+                text_color = 255, 255, 255
+            text = self.font.render(name_state, 1, text_color)
+            width_text, _ = text.get_size()
+            surface.blit(text, ((width - width_text) / 2, i*100))
+        pg.display.flip()
