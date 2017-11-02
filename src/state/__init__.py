dac1adb0fec016ee5d616a2c37527705b9d2d06c
Joseph Marotte <joseph.marotte@ens.fr>
2017-11-02 19:15:13 +0100

    oublie de rajouter l'init pour une commit précédent (celui où on modifie les import du main)

---
+++ src/state/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ["state_add_score",
+           "state_best_score",
+           "state_credits",
+           "state_engine",
+           "state_game_over",
+           "state_game",
+           "state_main_menu",
+           "state_options",
+           "state_pause"]
