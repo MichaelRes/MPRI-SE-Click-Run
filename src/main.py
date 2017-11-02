04721eefd91425b1a11baab687f1ec01669f2efe
Joseph Marotte <joseph.marotte@ens.fr>
2017-11-02 16:56:36 +0100

    import plus propre dans main

--- src/main.py
+++ src/main.py
@@ -1,34 +1,26 @@
 # -*- coding: utf-8 -*-
 import sys
 import pygame as pg
-#sys.path.append('state')
-from state.state_engine import *
-from state.state_main_menu import *
-from state.state_best_score import *
-from state.state_options import *
-from state.state_credits import *
-from state.state_game_over import *
-from state.state_game import *
-from state.state_pause import *
-from state.state_add_score import *
+from state import *
+
 
 def main():
     pg.init()
     screen = pg.display.set_mode((1200, 720))
     states = {
-        "MAIN_MENU": MainMenu(),
-        "BEST_SCORE": BestScore(),
-        "OPTIONS": Options(),
-        "CREDITS": Credits(),
-        "GAME_OVER": GameOver(),
-        "GAME": StateGame(),
-        "PAUSE": Pause(),
-        "ADD_SCORE": AddScore(),
+        "MAIN_MENU": state_main_menu.MainMenu(),
+        "BEST_SCORE": state_best_score.BestScore(),
+        "OPTIONS": state_options.Options(),
+        "CREDITS": state_credits.Credits(),
+        "GAME_OVER": state_game_over.GameOver(),
+        "GAME": state_game.StateGame(),
+        "PAUSE": state_pause.Pause(),
+        "ADD_SCORE": state_add_score.AddScore(),
             }
-    game = Game(screen, states, "MAIN_MENU")
+    game = state_engine.Game(screen, states, "MAIN_MENU")
     game.run()
     pg.quit()
     sys.exit()
 
-main()
 
+main()
