7e0472359c9836b37cb905e4f05e82e512df3c82
Joseph Marotte <joseph.marotte@ens.fr>
2017-11-17 19:25:28 +0100

    debut ajout mode deux joueurs, reste a faire en sorte qu'on puisse selectionner le nombre de joueur dans options, refactorisation de l'update du map qui va dans player pour la plupart des elements

--- src/state/state_game.py
+++ src/state/state_game.py
@@ -16,7 +16,10 @@ class StateGame(state_engine.GameState):
         """
         state_engine.GameState.__init__(self)
         self.current_opts = load_options()
-        self.player = Player(50, 0, 8, 0, self.current_opts["CHARACTER"])
+        self.player = Player(50, 0, 8, 0, self.current_opts["CHARACTER"], jump_key = pg.K_SPACE)
+        self.player2 = Player(150, 0, 8, 0, "mario", pg.K_SPACE)
+        self.player2.is_dead = True
+
         self.game_map = Map()
         self.acceleration_x = 0  # As said, x variables is not of any use at the moment
         self.acceleration_y = 1
@@ -38,24 +41,8 @@ class StateGame(state_engine.GameState):
                 self.next_state = "PAUSE"
                 self.persist["MAP"] = self.game_map
                 self.done = True
-        if event.type == pg.KEYDOWN:
-            # Let's try to make the player jump by modifiying its velocity after checking if it's on the ground
-            if event.key == pg.K_SPACE:
-                if self.game_map.object_on_the_ground(self.player):
-                    # TODO: pourquoi le -18 ici ? Le justifier et / ou le mettre en constante globale.
-                    # Il est ien comme ça ce petit -18.
-                    self.player.v_y = min(-18, self.player.v_y)
-                    # Player get an ascending phase that lasts some frame where he can still gain some vertical velocity
-                    self.player.action = Action.ASCEND
-                    self.player.last_jump = self.frame
-                elif self.player.action == Action.JUMPING and self.player.double_jump_available:
-                    self.player.double_jump_available = False
-                    self.player.v_y = -18
-                    self.player.action = Action.JUMPING
-        if event.type == pg.KEYUP:
-            if event.key == pg.K_SPACE:
-                if self.player.action == Action.ASCEND:
-                    self.player.action = Action.JUMPING
+        self.player.get_event(event, self.game_map)
+        self.player2.get_event(event, self.game_map)
 
     def update_score(self):
         """
@@ -72,17 +59,9 @@ class StateGame(state_engine.GameState):
         # Update of the pos
         x0 = self.player.pos_x
         self.score.update(self.frame)
-        is_the_game_over, (x, y) = self.game_map.move_test(self.player.pos_x,
-                                                           self.player.pos_y,
-                                                           self.player.hitbox,
-                                                           int (self.player.v_x * self.difficulty),
-                                                           int (self.player.v_y * self.difficulty))
-
-        # Because of the movement of the screen, we do not change the pos_x of the player : the screen will move later.
-        self.player.pos_y = y
 
         # Something to do in case the game is over
-        if is_the_game_over:
+        if self.player.is_dead and self.player2.is_dead:
             p = score.ScoreManager().pos_as_score(self.score)
             if p < score.ScoreManager().max_number_of_score:
                 self.persist = {"score": self.score, "pos": p}
@@ -93,23 +72,11 @@ class StateGame(state_engine.GameState):
                 self.next_state = "GAME_OVER"
             self.done = True
 
-        # Update depending on whether the player is on the ground or not
-        # This part should go in the game object class eventually
-
-        if self.game_map.object_on_the_ground(self.player) and self.player.action != Action.ASCEND:
-            self.player.action = Action.RUNNING
-            self.player.v_y = min(self.player.v_y, 0)
-            self.player.double_jump_available = True
-        elif self.player.action in [Action.JUMPING, Action.RUNNING] or \
-                (self.player.action == Action.ASCEND and self.frame - self.player.last_jump > 12):
-            # Either is the player in jump state, or he stopped his ascension
-            self.player.action = Action.JUMPING
-            self.player.v_y = max(min(self.player.v_y + self.difficulty*self.acceleration_y, self.max_speed), -self.max_speed)
-        elif self.player.action == Action.ASCEND:  # In that case, the player continues his ascension
-            self.player.v_y = max(min(self.player.v_y + self.difficulty*self.acceleration_y//2, self.max_speed), -self.max_speed)
+        self.player.update(self.game_map, self.difficulty, self.acceleration_y, self.max_speed)
+        self.player2.update(self.game_map, self.difficulty, self.acceleration_y, self.max_speed)
 
         # Update of the game_map
-        self.game_map.update(x - x0)
+        self.game_map.update(int(self.player.v_x * self.difficulty))
         self.persist["MAP"] = self.game_map
 
         # This part got to stay updated
@@ -132,5 +99,8 @@ class StateGame(state_engine.GameState):
         @rtype: None
         """
         self.game_map.display(surface)
+        #if not(self.player.is_dead):
         self.player.draw(surface)
+        #if not(self.player2.is_dead):
+        self.player2.draw(surface)
         self.score.draw(surface, self.font)
