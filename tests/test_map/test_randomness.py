6cc45982695890447c2a67096523ff9dbc1ef9c7
Hector <sacdesable123@gmail.com>
2017-11-27 09:28:13 +0100

    Ajout d'un générateur de pseudo-hasard déterminé par une seed pour la génération de la map, et ajout d'un test le concernant

---
+++ tests/test_map/test_randomness.py
@@ -0,0 +1,24 @@
+import os
+import sys
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../../src')
+from map import *
+
+def test_new_map():
+    """
+    This function should test that the randomness provided by the randint fuction of map
+    should not be absurd for some seed.
+    """
+    pg.init()
+    screen = pg.display.set_mode((1200, 720))
+    for i in range(100):
+        new_map = Map(i)
+        frequency = [0 for i in range(10)]
+        for j in range(350): # With 350, the probability that the test give a illegitimate error should be inferior to 10^-10.
+            frequency[new_map.randint(10)-1]+=1
+        for j in range(10):
+            assert frequency[j] != 0,"There's problem with random generation of the map for seed " + str(i)
+        print(frequency)
+    pg.quit()
+
+test_new_map()
