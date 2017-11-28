8c727493e01f2300100245a502a2a5e985555708
Hector <sacdesable123@gmail.com>
2017-11-28 14:17:08 +0100

    Prototype du replay.py

---
+++ src/replay.py
@@ -0,0 +1,61 @@
+from enum import Enum
+
+class ReplayMode(Enum):
+    WRITE = 1
+    READ = 2
+
+class Replay:
+    """
+    A class for replay.
+    """
+    def __init__(self, path= None):
+        """
+        If path isn't None, the replay should load the corresponding file.
+        Otherwise, the instance goes write mode on a new file
+        """
+        pass
+
+    def set_opts(self, options):
+        """
+        This should set the variable : difficulty and number of players in order to save them to the replay
+        """
+        pass
+    
+    def get_opts(self):
+        """
+        Give option in read mode
+        """
+        pass
+        
+    def load(self,path):
+        """
+        Should load from some file
+        """
+        pass
+    
+    def save(self,path):
+        """
+        write the replay in order to load it later
+        """
+        pass
+    
+    def is_empty(self):
+        """
+        return if this instance has been initialised
+        """
+        pass
+
+    def read(self,frame):
+        """
+        Return what happens at a given frame
+        Should happen only in read mode and in sequential order
+        """
+        return self.history == []
+    
+    def write(self, event, frame):
+        """
+        Write to the end of the file what's happening.
+        Should happen only in write mode
+        """
+        pass
+    
