2316fc586425f70bff57a2aae3ca3b48127afd6d
Hector <sacdesable123@gmail.com>
2017-11-30 14:16:24 +0100

    L'objet replay.

--- src/replay.py
+++ src/replay.py
@@ -1,8 +1,10 @@
 from enum import Enum
+import pickle
 
 class ReplayMode(Enum):
     WRITE = 1
     READ = 2
+
 
 class Replay:
     """
@@ -14,36 +16,49 @@ class Replay:
         Otherwise, the instance goes write mode on a new file
         """
         if self.path != None:
-            self.load(path)
             self.mode = ReplayMode.READ
+            self.load(path)
+            self.position = 0
         else:
-            self.history = []
             self.mode = ReplayMode.WRITE
+            self.history = []
             
     def set_opts(self, options):
         """
         This should set the variable : difficulty and number of players in order to save them to the replay
         """
-        pass
+        assert self.mode == ReplayMode.WRITE, "Wrong Mode for Replay class"
+        self.options = options
     
     def get_opts(self):
         """
         Give option in read mode
         """
-        pass
+        assert self.mode == ReplayMode.READ, "Wrong Mode for Replay class"
+        return self.options
         
     def load(self,path):
         """
         Should load from some file
         """
-        pass
+        assert self.mode == ReplayMode.READ, "Wrong Mode for Replay class"
+        f=open(path, "rb")
+        a=pickle.load(f, pickle.HIGHEST_PROTOCOL)
+        self.options = a[0]
+        self.history = a[1]
+
     
     def save(self,path):
         """
         write the replay in order to load it later
         """
-        pass
-    
+        assert self.mode == ReplayMode.WRITE, "Wrong Mode for Replay class"
+        with open(path, "wb") as f:
+            pickle.dump([self.options,self.history], f, pickle.HIGHEST_PROTOCOL)
+        
+        
+
+            
     def is_empty(self):
         """
         return if this instance has been initialised
@@ -55,12 +70,23 @@ class Replay:
         Return what happens at a given frame
         Should happen only in read mode and in sequential order
         """
-        pass
+        assert self.mode == ReplayMode.READ, "Wrong Mode for Replay class"
+        while 1:
+            if self.position >= 1 + len(self.history) or self.history[self.position][0] > frame:
+                return []
+            if self.history[self.position][0] == frame:
+                return self.history[self.position][1]
+            if self.history[self.position][0] < frame:
+                i+=1
+        
     
-    def write(self, event, frame):
+    def write(self, events, frame):
         """
         Write to the end of the file what's happening.
         Should happen only in write mode
         """
-        pass
-    
+        assert self.mode == ReplayMode.WRITE, "Wrong Mode for Replay class"
+        if self.history != [] and self.history[-1][0] == frame:
+            self.history[-1][1] += events
+        else:
+            self.history.append([frame, events])
