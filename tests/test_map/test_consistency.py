d25485b766aa0d13b4f81cfb99befbc4628839f3
Hector <sacdesable123@gmail.com>
2017-11-30 18:54:53 +0100

    Ajout d'un test pour s'assurer de la consistance de la génération en fonction de la seed

---
+++ tests/test_map/test_consistency.py
@@ -0,0 +1,24 @@
+import os
+import sys
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../../src')
+from map import *
+
+def test_new_map():
+    """
+    This function should test that the randomness provided by the randint fuction of map
+    should not be absurd for some seed.
+    """
+    pg.init()
+    screen = pg.display.set_mode((1200, 720))
+    for i in range(10):
+        map1 = Map(i*1000)
+        map2 = Map(i*1000)
+        for j in range(100):
+            map1.gen_one()
+            map2.gen_one()
+        for k in range(len(map1.data)):
+            for l in range(len(map1.data[0])):
+                assert map1.data[k][l] == map2.data[k][l], "Same seed should get same map"
+    pg.quit()
+
