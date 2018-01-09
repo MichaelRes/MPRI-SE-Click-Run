1be91d20fb8eba440603c1d39baa30f252182422
Hector <sacdesable123@gmail.com>
2018-01-09 19:42:11 +0100

    Correction du nom des fonctions des tests pour passer les tests

--- tests/test_map/test_gen_double_step.py
+++ tests/test_map/test_gen_double_step.py
@@ -4,11 +4,11 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../../src')
 from map import *
 
-def test_new_map():
+def test_double_step():
     """
     This function try to generate the double step pattern
     """
-    new_map = Map(None,)
+    new_map = Map(None,None)
     for i in range(1000):
         new_map.gen_double_step()
 
